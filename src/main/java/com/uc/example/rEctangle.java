c63800ddd62e9395302869b6b3d590e306de7e4d
rjvg19 <117688084+rjvg19@users.noreply.github.com>
2022-11-28 21:42:57 +0000

    Update rEctangle.java

--- src/main/java/com/uc/example/rEctangle.java
+++ src/main/java/com/uc/example/rEctangle.java
@@ -5,10 +5,10 @@ public class rEctangle {
     int w;
     int h;
 
-    public rEctangle(point origin, int w, int h) {
+    public rEctangle(point origin, int w2, int h2) {
         this.origin = origin;
-        this.w = w;
-        this.h = h;
+        this.w = w2;
+        this.h = h2;
     }
 
     public rEctangle(int w, int h) {

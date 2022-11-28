4b8ae7b1cbe544ff6a1422232936e60e37fdbb2a
Lukas Boschanski <lukas3b@googlemail.com>
2022-11-28 20:58:09 +0000

    balfdsaölk

--- src/main/java/com/uc/example/Hello.java
+++ src/main/java/com/uc/example/Hello.java
@@ -2,10 +2,10 @@ package com.uc.example;
 
 public class Hello {
     public static void main(String[] args) {
-        point originOne = new point(23, 94);
-        point OriginTWO = null;
-        rEctangle rectOne = new rEctangle(originOne, 100, 200);
-        rEctangle rectTwo = new rEctangle(OriginTWO, 50, 100);
+        Point originOne = new Point(23, 94);
+        Point OriginTWO = null;
+        Rectangle rectOne = new Rectangle(originOne, 100, 200);
+        Rectangle rectTwo = new Rectangle(OriginTWO, 50, 100);
 
         System.out.println("RectOne origin w:" + rectOne.origin.x);
         System.out.println("RectTwo origin X:" + rectTwo.origin.x);

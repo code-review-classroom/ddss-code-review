91b5c743fd64ba7ece4e11f04d3ce79ed1dc9c68
danielmarques <daniemarques@gmail.com>
2022-11-28 21:01:30 +0000

    Multiple fixes #3

--- src/main/java/com/uc/example/Hello.java
+++ src/main/java/com/uc/example/Hello.java
@@ -1,13 +1,17 @@
 package com.uc.example;
 
 public class Hello {
-    private static int xOriginOne = 23;
-    private static int yOriginOne = 94;
+    private static final int ORIGIN_ONE_X = 23;
+    private static final int ORIGIN_ONE_Y = 94;
+    private static final int RECT_ONE_W = 100;
+    private static final int RECT_ONE_H = 200;
+    private static final int RECT_TWO_W = 50;
+    private static final int RECT_TWO_H = 100;
     public static void main(String[] args) {
-        final Point originOne = new Point(xOriginOne, yOriginOne);
-        Point originTwo = null;
-        final Rectangle rectOne = new Rectangle(originOne, 100, 200);
-        final Rectangle rectTwo = new Rectangle(originTwo, 50, 100);
+        final Point originOne = new Point(ORIGIN_ONE_X, ORIGIN_ONE_Y);
+        final Point originTwo = null;
+        final Rectangle rectOne = new Rectangle(originOne, RECT_ONE_W, RECT_ONE_H);
+        final Rectangle rectTwo = new Rectangle(originTwo, RECT_TWO_W, RECT_TWO_H);
 
         System.out.println("RectOne origin w:" + rectOne.origin.x);
         System.out.println("RectTwo origin X:" + rectTwo.origin.x);
--- src/main/java/com/uc/example/Rectangle.java
+++ src/main/java/com/uc/example/Rectangle.java
@@ -1,16 +1,30 @@
 package com.uc.example;
 
+/**
+ * This is a Rectangle
+ */
 public class Rectangle {
     Point origin;
     int w;
     int h;
 
+    /**
+     * Creates a rectangle with a given point of origin
+     * @param origin point of origin
+     * @param w rectangle width
+     * @param h rectangle height
+     */
     public Rectangle(Point origin, int w, int h) {
         this.origin = origin;
         this.w = w;
         this.h = h;
     }
 
+    /**
+     * Creates a rectangle at position 0,0
+     * @param w rectangle width
+     * @param h rectangle height
+     */
     public Rectangle(int w, int h) {
         this.origin = new Point(0, 0);
         this.w = w;

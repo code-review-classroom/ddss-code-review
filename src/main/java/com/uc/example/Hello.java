c5d51d68e25c5855e89f6fde469b27d82c319474
Luís Ventura <92308003+lventura-codacy@users.noreply.github.com>
2023-07-20 18:46:56 +0100

    Update src/main/java/com/uc/example/Hello.java
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- src/main/java/com/uc/example/Hello.java
+++ src/main/java/com/uc/example/Hello.java
@@ -5,7 +5,17 @@ public class Hello {
         System.out.println("hello, world!");
         if ("aaa" == "bbb") 
             System.out.println("hey there");
-        if ("aaa" = "bbb") 
+        package com.uc.example;
+        
+        public class Hello {
+            public static void main(String[] args) {
+                System.out.println("hello, world!");
+                if ("aaa" == "bbb") 
+                    System.out.println("hey there");
+                if ("aaa" == "bbb") 
+                    System.out.println("poop");
+            }
+        }
             System.out.println("poop");
     }
 

f9e47130b859c767dc8a3cce96a70768b07bc4bb
Luís Ventura <92308003+lventura-codacy@users.noreply.github.com>
2023-07-20 11:01:49 +0100

    Testing

--- src/main/java/com/uc/example/Hello.java
+++ src/main/java/com/uc/example/Hello.java
@@ -3,5 +3,10 @@ package com.uc.example;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello, world!");
+        if ("aaa" == "bbb") 
+            System.out.println("hey there");
+        if ("aaa" = "bbb") 
+            System.out.println("poop");
     }
+
 }
